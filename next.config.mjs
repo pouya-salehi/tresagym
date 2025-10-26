3fa53d8ba69f17fe55a8bf088778431cc34f64b8
pouya-salehi <cooperstudio2024@gmail.com>
2025-10-26 15:33:28 +0330

    for depling on netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  // اگر از Image استفاده می‌کنی این رو اضافه کن
+  images: {
+    domains: ["www.weighttraining.guide"], // دامنه‌های عکس‌ها
+  },
+  // برای API routes
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "/api/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
